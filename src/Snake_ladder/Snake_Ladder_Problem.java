5eb1579c5e59d42db1d7fa8b9075ca0d411f9177
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 04:48:12 +0530

    Added:calculating total times dice rolled and every position of player

--- src/Snake_ladder/Snake_Ladder_Problem.java
+++ src/Snake_ladder/Snake_Ladder_Problem.java
@@ -10,30 +10,17 @@ public class Snake_Ladder_Problem {
 		final int LADDER=1;
 		final int SNAKE=2;
 		int playerPosition=0;
+		int diceRollCount=0;
 
 		//while loop to iterate till player reaches winning position 100
 		while(playerPosition <= 100)
 		{
 			//to get dice value
 			int dieValue=(int) ((Math.random() * 100) % 6)+1;
-			System.out.println("player gets "+dieValue);
+			diceRollCount++;
 
 			//to get status of player if its no play,ladder or snake
 			int playerStatus=(int) (Math.random() * 100) % 3;
-
-			//to print player status
-			if(playerStatus == 0) 
-			{
-				System.out.println("player is in No Play status ");
-			}
-			if(playerStatus == 1) 
-			{
-				System.out.println("player is in ladder status ");
-			}
-			if(playerStatus == 2) 
-			{
-				System.out.println("player is in snake status ");
-			}
 
 			//using switch case to check player position
 			switch(playerStatus)
@@ -43,6 +30,7 @@ public class Snake_Ladder_Problem {
 				break;
 			case LADDER:
 				playerPosition +=dieValue;
+
 				break;
 			default:
 				playerPosition -=dieValue;
@@ -69,6 +57,8 @@ public class Snake_Ladder_Problem {
 			if(playerPosition == 100)
 			{
 				System.out.println("player reaches winning position "+playerPosition);
+				System.out.println("total number of times dice rolled is "+diceRollCount);
+
 				break;
 			}
 		}
