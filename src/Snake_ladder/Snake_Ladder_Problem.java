e171c7b686f02255d30219eb24f087c97453225f
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 03:21:24 +0530

    Added:showing starting position of player

--- src/Snake_ladder/Snake_Ladder_Problem.java
+++ src/Snake_ladder/Snake_Ladder_Problem.java
@@ -3,6 +3,9 @@ package Snake_ladder;
 public class Snake_Ladder_Problem {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to snake ladder problem");
+		final int START=0;
+
+		System.out.println("starting position of player is  " + START);
+
 	}
 }
