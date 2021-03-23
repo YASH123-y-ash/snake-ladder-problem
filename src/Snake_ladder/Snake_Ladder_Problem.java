d41fe376e048cfa2de056a3b069e07b2dba20ca8
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 04:24:32 +0530

    Added:repeat till player position reaches 100

--- src/Snake_ladder/Snake_Ladder_Problem.java
+++ src/Snake_ladder/Snake_Ladder_Problem.java
@@ -11,41 +11,59 @@ public class Snake_Ladder_Problem {
 		final int SNAKE=2;
 		int playerPosition=0;
 
-		//using random function to get dice value
-		int dieValue=(int) ((Math.random() * 100) % 6)+1;
-		System.out.println("player gets "+dieValue);
-
-		//using random function to get status of player if its no play,ladder or snake
-		int playerStatus=(int) (Math.random() * 100) % 3;
-
-		//to print player status
-		if(playerStatus == NO_PLAY) 
+		//while loop to iterate till player reaches winning position 100
+		while(playerPosition <= 100)
 		{
-			System.out.println("player is in No Play status ");
-		}
-		if(playerStatus == LADDER) 
-		{
-			System.out.println("player is in ladder status ");
-		}
-		if(playerStatus == SNAKE) 
-		{
-			System.out.println("player is in snake status ");
-		}
+			//to get dice value
+			int dieValue=(int) ((Math.random() * 100) % 6)+1;
+			System.out.println("player gets "+dieValue);
 
-		//checking player position
-		switch(playerStatus)
-		{
-		case NO_PLAY:
-			playerPosition =playerPosition;
-			break;
-		case LADDER:
-			playerPosition +=dieValue;
-			break;
-		default:
-			playerPosition -=dieValue;
-			break;
-		}
-		System.out.println("player position is "+playerPosition);
+			//to get status of player if its no play,ladder or snake
+			int playerStatus=(int) (Math.random() * 100) % 3;
 
+			//to print player status
+			if(playerStatus == 0) 
+			{
+				System.out.println("player is in No Play status ");
+			}
+			if(playerStatus == 1) 
+			{
+				System.out.println("player is in ladder status ");
+			}
+			if(playerStatus == 2) 
+			{
+				System.out.println("player is in snake status ");
+			}
+
+			//using switch case to check player position
+			switch(playerStatus)
+			{
+			case 0:
+				playerPosition =playerPosition;
+				break;
+			case 1:
+				playerPosition +=dieValue;
+				break;
+			default:
+				playerPosition -=dieValue;
+				break;
+			}
+
+			System.out.println("player position is "+playerPosition);
+
+			//checking if player position less than 0
+			if(playerPosition < 0)
+			{
+				playerPosition =0;
+				System.out.println("player position is "+playerPosition);
+			}
+
+			//checking if player position reaches 100
+			if(playerPosition == 100)
+			{
+				System.out.println("player reaches winning position "+playerPosition);
+				break;
+			}
+		}
 	}
 }
