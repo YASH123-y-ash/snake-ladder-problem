a443bd2a90f626878e62c8180965cd1c7633f265
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 17:13:57 +0530

    Refactored:package name and class name

---
+++ src/com/snakeladder/SnakeLadderProblem.java
@@ -0,0 +1,10 @@
+package com.snakeladder;
+
+public class SnakeLadderProblem {
+
+	public static void main(String[] args) {
+		final int START=0;
+
+		System.out.println("starting position of player is  " + START);
+	}
+}
