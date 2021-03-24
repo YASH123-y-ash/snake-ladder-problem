e001744a8afcfb02f05a900d5d5688ef234a3642
Yash-y-ash <wardhan696@gmail.com>
2021-03-24 19:29:10 +0530

    Refactored:package and class name modified

--- src/com/snakeladder/SnakeLadderProblem.java
+++ src/com/snakeladder/SnakeLadderProblem.java
@@ -1,9 +1,9 @@
-package Snake_ladder;
+package com.snakeladder;
 
-public class Snake_Ladder_Problem {
+public class SnakeLadderProblem {
 
 	public static void main(String[] args) {
-		
+
 		int dieValue=(int) ((Math.random() * 100) % 6)+1;
 		System.out.println("player gets "+dieValue);
 	}
